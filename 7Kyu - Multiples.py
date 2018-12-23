7ef6641a148dc01e040b2f59527c119288edc90d
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-23 16:06:20 +0530

    Create 7Kyu - Multiples.py

---
+++ 7Kyu - Multiples.py
@@ -0,0 +1,10 @@
+def multiple(x):
+    #Good Luck 
+    if(x%5==0 and x%3==0):
+        return 'BangBoom'
+    elif(x%5==0):
+        return 'Boom'
+    elif(x%3==0):
+        return 'Bang'
+    else:    
+        return 'Miss'
