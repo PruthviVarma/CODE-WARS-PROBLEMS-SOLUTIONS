82c060a65fa93ac2d6c393d4735e6fee21ec77b0
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-23 16:15:18 +0530

    Create 8Kyu - Twice as old.py

---
+++ 8Kyu - Twice as old.py
@@ -0,0 +1,5 @@
+def twice_as_old(dad_years_old, son_years_old):
+    if(2*son_years_old>dad_years_old):
+        return (2*son_years_old)-(dad_years_old)
+    else:
+        return (dad_years_old) - (2*son_years_old)
