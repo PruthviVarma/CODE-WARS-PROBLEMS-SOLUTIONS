77dc3d309d568dad0729d7ebce82b52ad696dbdc
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-24 07:55:22 +0530

    Create 8Kyu - Thinkful - Logic Drills: Traffic light.py

---
+++ 8Kyu - Thinkful - Logic Drills: Traffic light.py
@@ -0,0 +1,8 @@
+def update_light(current):
+    # Your code here.
+    if(current=='green'):
+        return 'yellow'
+    elif(current=='yellow'):
+        return 'red'
+    else:
+        return 'green'
