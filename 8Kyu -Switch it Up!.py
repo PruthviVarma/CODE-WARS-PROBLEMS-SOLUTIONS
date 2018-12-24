0e86d61477c144164bfe4957b8980343e7e15b1f
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-24 08:36:14 +0530

    Create 8Kyu -Switch it Up!.py

---
+++ 8Kyu -Switch it Up!.py
@@ -0,0 +1,22 @@
+def switch_it_up(number):
+    #your code here
+    if number == 1:
+        return "One"
+    elif number == 2:
+        return "Two"
+    elif number == 3:
+        return "Three"
+    elif number == 4:
+        return "Four"
+    elif number == 5:
+        return "Five"
+    elif number == 6:
+        return "Six"
+    elif number == 7:
+        return "Seven"
+    elif number == 8:
+        return "Eight"
+    elif number == 9:
+        return "Nine"
+    else:
+        return "Zero"
