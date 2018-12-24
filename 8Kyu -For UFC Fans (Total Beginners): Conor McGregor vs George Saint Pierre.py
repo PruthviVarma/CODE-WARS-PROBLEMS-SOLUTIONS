879603b0ba56c88f94384ab158125bdac5364432
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-24 08:41:09 +0530

    Create 8Kyu -For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py

---
+++ 8Kyu -For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py
@@ -0,0 +1,3 @@
+def quote(fighter):
+    pass
+    return "I am not impressed by your performance." if(fighter.lower()=='george saint pierre') else "I'd like to take this chance to apologize.. To absolutely NOBODY!"
