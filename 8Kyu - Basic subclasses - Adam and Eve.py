b97b96ed5d323e8105a1d4c9a6a1cd6c4958cf99
Nagendra <44141606+nagendra226@users.noreply.github.com>
2018-12-25 16:14:13 +0530

    Create 8Kyu - Basic subclasses - Adam and Eve.py

---
+++ 8Kyu - Basic subclasses - Adam and Eve.py
@@ -0,0 +1,14 @@
+class  Human:
+    pass
+class Man(Human):
+    pass
+class Woman(Human):
+    pass
+    
+def God():
+    #code
+    Adam = Man()
+    Eve = Woman()
+    return [Adam,Eve]
+
+#code
